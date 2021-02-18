bbea24a3981123a3c96879b99c1c6ec95afec7db
nykw <nykw196884@gmail.com>
2021-02-18 16:04:35 +0900

    ユーザーの説明ページにGitHubの情報を表示できるようにしました。

---
+++ hooks/useUser.ts
@@ -0,0 +1,41 @@
+export type User = {
+  avatar_url: string | undefined;
+  bio: string | undefined;
+  blog: string | undefined;
+  company: string | undefined;
+  created_at: string | undefined;
+  email: string | undefined;
+  events_url: string | undefined;
+  followers: number | undefined;
+  followers_url: string | undefined;
+  following: number | undefined;
+  following_url: string | undefined;
+  gists_url: string | undefined;
+  gravatar_id: string | undefined;
+  hireable: boolean | undefined;
+  html_url: string | undefined;
+  id: number | undefined;
+  location: string | undefined;
+  login: string | undefined;
+  name: string | undefined;
+  node_id: string | undefined;
+  organizations_url: string | undefined;
+  public_gists: number | undefined;
+  public_repos: number | undefined;
+  received_events_url: string | undefined;
+  repos_url: string | undefined;
+  site_admin: boolean | undefined;
+  starred_url: string | undefined;
+  subscriptions_url: string | undefined;
+  twitter_username: string | undefined;
+  type: string | undefined;
+  updated_at: string | undefined;
+  url: string | undefined;
+};
+
+export default async function useUser(user: string) {
+  const res = await fetch(`https://api.github.com/users/${user}`);
+  const data: User | undefined = await res.json();
+
+  return data;
+}
--- pages/users/[user].tsx
+++ pages/users/[user].tsx
@@ -1,26 +1,52 @@
 import { FC } from 'react';
 import Template from '../../components/template';
-import { useRouter } from 'next/router';
-import useUser, { User } from '../api/user';
+import useUser, { User } from '../../hooks/useUser';
 import { GetServerSideProps } from 'next';
 
+type Props = User & {
+  name: string;
+  followers: number;
+  following: number;
+  email: string | undefined;
+  location: string | undefined;
+  twitter_username: string | undefined;
+};
+
 export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { params } = context;
+  if (!params)
+    return {
+      notFound: true,
+    };
+
+  const { user } = params;
+  if (!user)
+    return {
+      notFound: true,
+    };
+
+  const data = await useUser(user as string);
+  if (!data)
+    return {
+      notFound: true,
+    };
+
   return {
-    props: {},
+    props: data,
   };
 };
 
-const Content = async (user: string) => {
-  const { data, isLoading, isError } = useUser(user);
-  if (isLoading) return <div>Spinner</div>;
-  if (isError) return <div>Error</div>;
-  return <div>{data}</div>;
-};
-
-const Page: FC = () => {
-  const router = useRouter();
-  const { user } = router.query;
-  return <Template title={`${user}'s Page`}>{Content(user as string)}</Template>;
+const Page: FC<Props> = ({ name, followers, following, email, location, twitter_username }) => {
+  return (
+    <Template title={`${name}'s Page`}>
+      <p>name:{name}</p>
+      <p>followers:{followers}</p>
+      <p>following:{following}</p>
+      <p>email:{email}</p>
+      <p>location:{location}</p>
+      <p>twitter_username:{twitter_username}</p>
+    </Template>
+  );
 };
 
 export default Page;
