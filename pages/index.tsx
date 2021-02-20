20fedaf68a2da65051e4bde4c4a195826bd4af34
nykw <nykw196884@gmail.com>
2021-02-20 15:59:20 +0900

    不正なユーザー名であることの警告をsubmitボタン押下時に変更しました。

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,28 @@ import Template from '../components/template';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 
-/** 入力したユーザー名のバリデーション */
+/** 入力したユーザー名が正しい形式かを評価する。 */
 const validateUserName = (inputUserName: string): boolean =>
   inputUserName.length > 0 && inputUserName.split('/').length === 1;
 
 export default function Index() {
-  const [user, setUser] = useState('');
+  const [user, setUser] = useState(''); // 入力されたユーザー名
   const [validUserName, setValidUserName] = useState(validateUserName(user)); // 入力されたユーザー名が正しい形式かどうかのフラグ
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  /** ユーザー名編集のイベントハンドラー */
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setUser(e.target.value);
   };
 
+  /** submit ボタンを押下のイベントハンドラー */
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>): void => {
+    if (!validUserName) {
+      e.preventDefault();
+      alert('不正なユーザー名です。');
+    }
+  };
+
+  // ユーザー名が変化したときに正しい形式かどうかのフラグを評価する
   useEffect(() => {
     setValidUserName((_) => validateUserName(user));
   }, [user]);
@@ -34,24 +44,19 @@ export default function Index() {
           </label>
           <Link href={`/users/${user}`}>
             <button
-              type="submit"
+              type="button"
               className="h-10 w-20 rounded-md bg-blue-600"
-              disabled={!validUserName}
+              onClick={handleClick}
             >
               <span className="font-bold text-white">Submit</span>
             </button>
           </Link>
-          {!validUserName && (
-            <p>
-              <span className="font-bold text-red-500">不正なユーザー名です。</span>
-            </p>
-          )}
         </form>
         <div>
           <h2>機能</h2>
-          <p>
+          <div className="w-300">
             GitHubのユーザー名を入力してSubmitボタンを押すと、そのユーザーの説明ページにリンクします。
-          </p>
+          </div>
         </div>
       </div>
     </Template>
