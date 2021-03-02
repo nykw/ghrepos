e18ccaa8f2f1a1788bf71fb14355cdfdd3567004
nykw <nykw196884@gmail.com>
2021-03-02 19:49:34 +0900

    アカウント削除時にFirestoreからデータが削除されるようになりました。

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,15 +3,33 @@ import admin from "firebase-admin";
 admin.initializeApp();
 
 // ユーザー作成時に関数をトリガーする
-export const createCollectionForUser = functions
+export const createUserDoc = functions
     .region("asia-northeast1")
     .auth.user()
     .onCreate(async (user) => {
       if (user.displayName) {
+        const {uid, displayName} = user;
+
         const writeResult = await admin
             .firestore()
             .collection("users")
-            .add({displayName: user.displayName});
+            .doc(displayName)
+            .set({displayName, uid});
+
+        functions.logger.log(`writeResult: ${writeResult}`);
+      }
+    });
+
+// ユーザー削除時に関数をトリガーする
+export const deleteUserDoc = functions
+    .region("asia-northeast1")
+    .auth.user()
+    .onDelete(async (user) => {
+      if (user.displayName) {
+        const {displayName} = user;
+
+        const writeResult = await admin.
+            firestore().collection("users").doc(displayName).delete();
 
         functions.logger.log(`writeResult: ${writeResult}`);
       }
